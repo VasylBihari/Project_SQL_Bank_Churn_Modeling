b36d610f21caec10d93475a690a64363d215ce8a
VasylBihari <vasylbihari@gmail.com>
2025-06-15 11:37:17 +0300

    Update sql_scripts.sql

--- sql/sql_scripts.sql
+++ sql/sql_scripts.sql
@@ -1,1 +1,27 @@
+/*Створюю таблицю в PostgreSQL*/
 
+CREATE TABLE bank_churn (
+	CustomerId INTEGER PRIMARY KEY,
+    Surname VARCHAR(50),
+    CreditScore INTEGER,
+    Geography VARCHAR(50),
+    Gender VARCHAR(10),
+    Age INTEGER,
+    Tenure INTEGER,
+    Balance NUMERIC(15, 2),
+    NumOfProducts INTEGER,
+    HasCrCard BOOLEAN,
+    IsActiveMember BOOLEAN,
+    EstimatedSalary NUMERIC(15, 2),
+    Exited BOOLEAN
+);
+
+/*З допомогою опції Import/Export Data... імпортую CSV-файл у таблицю*/
+
+/*Перевіряю чи таблиця створена і дані імпортовані.*/
+
+SELECT * 
+FROM bank_churn
+LIMIT 5;
+
+/**/
